d9deeec6a65fba3a990d2b6b036813b2d9e59fa3
fokinkir96 <fokinkir96@gmail.com>
2023-03-12 22:16:05 +0300

    Added Server class

--- server.py
+++ server.py
@@ -2,17 +2,57 @@ import socket
 
 sock = socket.socket()
 
-sock.bind(('', 9090))
+sock.bind(('localhost', 9090))
 
-sock.listen(1)
+sock.listen()
 
-# print(addr)
+print(sock.getsockname()[0])
+print(sock.getsockname()[1])
 
 while True:
     conn, addr = sock.accept()
+    print('conn:', conn)
+    print('addr:', addr)
+    print('sock:', sock)
     data = conn.recv(1024)
-    if not data:
-        break
+    print('data:', data)
+    # if not data:
+    #     break
     conn.send(data.upper())
 
 # conn.close()
+# class Server:
+#
+#     def __init__(self):
+#         self.sock = self.start()
+#
+#     def start(self, host = '', port = 9090):
+#         sock = socket.socket()
+#
+#         sock.bind((host, port))
+#
+#         print('Сервер запущен')
+#
+#         return sock
+#
+#     def listen_port(self, quantity = 1):
+#         sock.listen(quantity)
+#         print('Слушаем порт')
+#         print(self.sock)
+#
+#     def cl_connect(self):
+#         conn, addr = sock.accept()
+#         print('Клиент подключился')
+#
+#     def get_data(self):
+#         print('Получили: ')
+#
+#     def send_data(self):
+#         print('Отправили: ')
+#
+#     def cl_disConnect(self):
+#         print('Клиент отключился')
+#
+#     def stop(self):
+#         print('Сервер остановлен')
+#
