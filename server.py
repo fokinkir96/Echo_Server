719863334d64ed638d9b31699e3d9e0beb11b739
fokinkir96 <fokinkir96@gmail.com>
2023-03-12 20:04:17 +0300

    Added dont close connection

--- server.py
+++ server.py
@@ -6,14 +6,13 @@ sock.bind(('', 9090))
 
 sock.listen(1)
 
-conn, addr = sock.accept()
-
 # print(addr)
 
 while True:
+    conn, addr = sock.accept()
     data = conn.recv(1024)
     if not data:
         break
     conn.send(data.upper())
 
-conn.close()
+# conn.close()
