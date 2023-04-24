fcc58e708c438499774f36ba01c1b6b60175bee4
fokinkir96 <fokinkir96@gmail.com>
2023-04-24 21:57:31 +0300

    Adding 1-5 points
    Проверьте возможность подключения к серверу с локальной, виртуальной и удаленной машины.
    Модифицируйте код сервера таким образом, чтобы он читал строки в цикле до тех пор, пока клиент не введет “exit”. Можно считать, что это команда разрыва соединения со стороны клиента.
    Модифицируйте код сервера таким образом, чтобы при разрыве соединения клиентом он продолжал слушать данный порт и, таким образом, был доступен для повторного подключения.
    Модифицируйте код клиента и сервера таким образом, чтобы номер порта и имя хоста (для клиента) они спрашивали у пользователя. Реализовать безопасный ввод данных и значения по умолчанию.
    Модифицировать код сервера таким образом, чтобы все служебные сообщения выводились не в консоль, а в специальный лог-файл.
    
    https://github.com/fa-python-network/1_echo_server/blob/master/README.md

--- client.py
+++ client.py
@@ -1,13 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import socket
 
-sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send('hello, world!'.encode('UTF-8'))
+class Client:
+    def __init__(self, conn='', addr=''):
+        self.conn = socket.socket()
+        self.addr = addr
+    def connect(self, host='localhost', port=9090):
+        self.conn.connect((host, port))
+    def recv(self, bytes = 1024):
+        self.data = data = self.conn.recv(bytes).decode()
+        print('Получили: '+self.data)
+
+        return data
+
+    def send(self, d=''):
+        data = d
+        if d == '':
+            data = self.data
+        self.conn.send(data.encode('UTF-8'))
+        print('Отправили: '+data)
+
+    def disConnect(self):
+        self.send('exit')
+        print('Клиент отключился')
+
+
+sock = Client()
+
+host = input('Введите хост(def: localhost): ')
+host = host if host != '' else 'localhost'
+port = input('Введите порт(def: 9090): ')
+port = port if port != '' else 9090
+
+sock.connect(host, port)
+
+sock.send('hello, world!')
 
 data = sock.recv(1024)
-sock.close()
+sock.disConnect()
 
 print(data)
--- server.py
+++ server.py
@@ -1,58 +1,127 @@
-import socket
+import socket, os, datetime
 
-sock = socket.socket()
-
-sock.bind(('localhost', 9090))
-
-sock.listen()
-
-print(sock.getsockname()[0])
-print(sock.getsockname()[1])
-
-while True:
-    conn, addr = sock.accept()
-    print('conn:', conn)
-    print('addr:', addr)
-    print('sock:', sock)
-    data = conn.recv(1024)
-    print('data:', data)
-    # if not data:
-    #     break
-    conn.send(data.upper())
+# sock = socket.socket()
+#
+# sock.bind(('localhost', 9090))
+#
+# sock.listen()
+#
+# print(sock.getsockname()[0])
+# print(sock.getsockname()[1])
+#
+# while True:
+#     conn, addr = sock.accept()
+#     print('conn:', conn)
+#     print('addr:', addr)
+#     print('sock:', sock)
+#     data = conn.recv(1024)
+#     print('data:', data)
+#     # if not data:
+#     #     break
+#     conn.send(data.upper())
 
 # conn.close()
-# class Server:
-#
-#     def __init__(self):
-#         self.sock = self.start()
-#
-#     def start(self, host = '', port = 9090):
-#         sock = socket.socket()
-#
-#         sock.bind((host, port))
-#
-#         print('Сервер запущен')
-#
-#         return sock
-#
-#     def listen_port(self, quantity = 1):
-#         sock.listen(quantity)
-#         print('Слушаем порт')
-#         print(self.sock)
-#
-#     def cl_connect(self):
-#         conn, addr = sock.accept()
-#         print('Клиент подключился')
-#
-#     def get_data(self):
-#         print('Получили: ')
-#
-#     def send_data(self):
-#         print('Отправили: ')
-#
-#     def cl_disConnect(self):
-#         print('Клиент отключился')
-#
-#     def stop(self):
-#         print('Сервер остановлен')
-#
+# TODO:
+#    Модифицируйте код сервера таким образом, чтобы он автоматически изменял номер порта, если он уже занят. Сервер должен выводить в консоль номер порта, который он слушает.
+#    Обработка ввода команд и общения сервер<->клиент
+
+class Serv:
+
+    def __init__(self):
+        self.log_file = 'logs/server.log'
+
+    def create_log_file(self):
+        if 'logs' not in os.listdir():
+            os.mkdir('logs')
+        log = open(self.log_file, 'a', encoding='UTF-8')
+        print(log)
+
+        return log
+
+    def add_log(self, log):
+        print(log)
+        with open(self.log_file, 'a', encoding='UTF-8') as f:
+            f.write(str(datetime.datetime.now())+': '+log+'\n')
+
+class Client(Serv):
+    def __init__(self, conn='', addr=''):
+        super().__init__()
+        self.conn = conn if conn != '' else socket.socket()
+        self.addr = addr
+        name = self.get_client_name(self.addr[0])
+        if name == False:
+            self.send('Введите свое имя: ')
+            #
+    def connect(self, host='localhost', port=9090):
+        self.conn.connect((host, port))
+    def recv(self, bytes = 1024):
+        self.data = data = self.conn.recv(bytes).decode()
+        self.add_log('Получили: '+self.data)
+
+        return data
+
+    def send(self, d=''):
+        data = d
+        if d == '':
+            data = self.data
+        self.conn.send(data.encode('UTF-8'))
+        self.add_log('Отправили: '+data)
+
+    def get_client_name(self, ip):
+        with open('serv/clients.txt', 'r') as f:
+            for i in f.readlines():
+                i = i.split()
+                if ip == i[0]:
+                    return i[1]
+
+        return False
+    def disConnect(self):
+        # self.send('exit')
+        self.add_log('Клиент '+str(self.addr[0])+':'+str(self.addr[1])+' отключился')
+
+
+class Server(Serv):
+
+    def __init__(self, host = 'localhost', port = 80, quantity = 5):
+        super().__init__()
+        self.sock = self.start()
+        self.host = host
+        self.port = port
+        self.quantity = quantity
+        self.connected = []
+
+    def start(self):
+        sock = socket.socket()
+
+        self.add_log('Сервер запущен')
+
+        return sock
+
+    def listen_port(self):
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(self.quantity)
+        self.add_log('Слушаем '+str(self.host)+' на '+str(self.port)+' порту')
+        # print(self.sock)
+
+    def wait_client(self):
+        c, addr = self.sock.accept()
+        cl = Client(c, addr)
+        self.connected.append(cl)
+
+        self.add_log('Клиент '+str(cl.addr[0])+':'+str(cl.addr[1])+' подключился')
+        return cl
+    def stop(self):
+        self.add_log('Сервер остановлен')
+        exit()
+
+
+s = Server()
+s.listen_port()
+while True:
+    client = s.wait_client()
+    data = ''
+    while data != 'exit':
+        data = client.recv(1024)
+        client.send(data)
+    else:
+        client.disConnect()
